6057b1923f7106b40d9017dabfea2e4bdba3ebf7
Tuigun Bekishev <tbekishev@gmail.com>
2023-02-13 17:36:55 -0400

    add new components

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,11 @@ const Home = () => {
   return (
     <>
       HeroBanner
-      <div>
+      <div className='products-heading'>
         <h2>Best selling products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>
+      <div className='products-container'>
         {['Product 1', 'Product 2'].map((product) => product)}
       </div>
       Footer
