c1295db8598e08097e4e4c552ff89c88aba63d55
Tuigun Bekishev <tbekishev@gmail.com>
2023-02-14 18:56:57 -0400

    add slug styles

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -21,7 +21,7 @@ const ProductDetails = ({ product, products }) => {
             ))}
           </div> */}
         </div>
-        <div className='product-details-desc'>
+        <div className='product-detail-desc'>
           <h1>{name}</h1>
           <div className='reviews'>
             <div>
