d8332a68774c6b7dc4e490eeadefcb39ed63fa2b
Tuigun Bekishev <tbekishev@gmail.com>
2023-02-14 17:01:08 -0400

    add footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { AiFillInstagram, AiFillTwitterCircle } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='footer-container'>
+      <p>2023 Tuigun's Headphones (c)</p>
+      <p className='icons'>
+        <AiFillInstagram/>
+        <AiFillTwitterCircle/>
+      </p>
     </div>
   )
 }
