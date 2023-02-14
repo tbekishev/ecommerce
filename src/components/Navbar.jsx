22b9e25f556263b78238536d6d9ce1354a6b4deb
Tuigun Bekishev <tbekishev@gmail.com>
2023-02-14 16:48:23 -0400

    install react-icons and add shopping button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
+import Link from 'next/link';
 import React from 'react';
+import { AiOutlineShopping } from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>Tuigun's Headphones</Link>
+      </p>
+      <button type='button' className='cart-icon'>
+        <AiOutlineShopping/>
+        <span className='cart-item-qty'>2</span>
+      </button>
     </div>
   )
 }
